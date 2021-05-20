7dd4c2958db3f29cf27ec246cab79ff1e0f738bf
RenataCehajic <renata.cehajic@gmail.com>
2021-05-20 17:30:58 +0200

    change the link

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,3 @@
 export const apiUrl =
-  process.env.API_URL || "https://walking-tour-app.herokuapp.com/";
+  process.env.API_URL || "https://walking-tour-app.herokuapp.com";
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
